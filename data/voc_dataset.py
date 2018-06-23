ec72bdb2ecacadbfd87eedc095fbac84d3a49a77
xingye.tian <xingye.tian@zhangmen.com>
2018-06-23 17:28:39 +0800

    [VOC_BBOX_LABEL_NAMES] changed VOC_BBOX_LABEL_NAMES when using question dataset.

--- data/voc_dataset.py
+++ data/voc_dataset.py
@@ -139,24 +139,26 @@ class VOCBboxDataset:
     __getitem__ = get_example
 
 
-VOC_BBOX_LABEL_NAMES = (
-    'aeroplane',
-    'bicycle',
-    'bird',
-    'boat',
-    'bottle',
-    'bus',
-    'car',
-    'cat',
-    'chair',
-    'cow',
-    'diningtable',
-    'dog',
-    'horse',
-    'motorbike',
-    'person',
-    'pottedplant',
-    'sheep',
-    'sofa',
-    'train',
-    'tvmonitor')
+# VOC_BBOX_LABEL_NAMES = (
+#     'aeroplane',
+#     'bicycle',
+#     'bird',
+#     'boat',
+#     'bottle',
+#     'bus',
+#     'car',
+#     'cat',
+#     'chair',
+#     'cow',
+#     'diningtable',
+#     'dog',
+#     'horse',
+#     'motorbike',
+#     'person',
+#     'pottedplant',
+#     'sheep',
+#     'sofa',
+#     'train',
+#     'tvmonitor')
+
+VOC_BBOX_LABEL_NAMES = ('text')
